095e9ce8b3e07ce1066b527dfd1ea8ae72f92a16
Michał Siedlaczek <michal.siedlaczek@gmail.com>
2019-05-07 14:55:05 -0400

    Support WaPo and NYT (#8)

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 FROM pisa/pisa
 
+COPY setup.sh setup.sh
+RUN bash setup.sh
+
 COPY init init
 COPY index index
 COPY search search
 
 WORKDIR /work
-
---
+++ setup.sh
@@ -0,0 +1,4 @@
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sh Miniconda3-latest-Linux-x86_64.sh -b
+git clone https://github.com/pisa-engine/nyt-corpus-reader.git
+/root/miniconda3/bin/pip install -r nyt-corpus-reader/requirements.txt
